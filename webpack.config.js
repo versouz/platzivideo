40cb75f960190fd461c2e9c8450ebf9656d4f583
Versouzz <versouzz@gmail.com>
2019-10-12 21:50:14 +0200

    Add README

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,10 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
@@ -17,36 +17,36 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_module/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader'
-          }
-        ]
+            loader: 'html-loader',
+          },
+        ],
       },
       {
         test: /\.(s*)css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
           },
           'css-loader', // Translate CSS into CommonJS
-          'sass-loader' // Compiles Sass to CSS
-        ]
-      }
-    ]
+          'sass-loader', // Compiles Sass to CSS
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].css'
-    })
-  ]
+      filename: 'assets/[name].css',
+    }),
+  ],
 };
